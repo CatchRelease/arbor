6416bc5f1888b19c0d54c9b7b3a17fd113858ecc
Damian Galarza <damian@catchandrelease.tv>
2018-10-23 11:11:04 -0400

    Support textAlign and space in Icon component

--- src/Icon.js
+++ src/Icon.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import styled, { cx } from 'react-emotion';
-import { color, fontSize } from 'styled-system';
+import { color, fontSize, textAlign, space } from 'styled-system';
 import { withTheme } from 'emotion-theming';
 import PropTypes from 'prop-types';
 
 const StyledIcon = styled.i`
   ${color};
   ${fontSize};
+  ${space};
+  ${textAlign};
 `;
 
 StyledIcon.propTypes = {
   ...color.propTypes,
-  ...fontSize.propTypes
+  ...fontSize.propTypes,
+  ...space.propTypes,
+  ...textAlign.propTypes
 };
 
 export const Icon = ({ name, theme, ...props }) => {
