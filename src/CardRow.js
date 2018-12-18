5d6313e1ba8269451ce1df3ade026083dd7e14c0
Euan Lau <euan@catchandrelease.tv>
2018-12-18 13:40:34 -0800

    Simplify the implementation of CardRow with withComponent

--- src/CardRow.js
+++ src/CardRow.js
@@ -1,18 +1,13 @@
-import React from 'react';
-
 import Flex from './Flex';
 
-const CardRow = ({ props, children }) => (
-  <Flex
-    px="regular"
-    py="smallest"
-    my="smaller"
-    alignItems="center"
-    justifyContent="space-between"
-    {...props}
-  >
-    {children}
-  </Flex>
-);
+const CardRow = Flex.withComponent('div');
+
+CardRow.defaultProps = {
+  px: 'regular',
+  py: 'smallest',
+  my: 'smaller',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+};
 
 export default CardRow;
