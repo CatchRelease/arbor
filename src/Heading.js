3969263c32d8850ad058e145ebebeada7bed8df4
Damian Galarza <damian@catchandrelease.tv>
2018-12-20 14:08:33 -0500

    Add styled-system fontWeight to Text component
    [#162794851]

--- src/Heading.js
+++ src/Heading.js
@@ -6,7 +6,6 @@ import Text from './Text';
 const lineHeightMultiplier = 0.5;
 
 const baseStyles = ({ theme }) => css`
-  font-weight: ${theme.fontWeights.bold};
   line-height: ${theme.lineHeights.small};
 `;
 
@@ -21,52 +20,60 @@ const Heading = styled(Text.withComponent('h1'))`
   ${calculatedMargin};
 `;
 
+Heading.defaultProps = {
+  fontWeight: 'bold'
+};
+
 Heading.h1 = Heading.withComponent('h1');
 
 Heading.h1.defaultProps = {
   color: 'grey100',
-  fontSize: 'size7'
+  fontSize: 'size7',
+  ...Heading.defaultProps
 };
 
 Heading.h2 = Heading.withComponent('h2');
 
 Heading.h2.defaultProps = {
   color: 'grey80',
-  fontSize: 'size6'
+  fontSize: 'size6',
+  ...Heading.defaultProps
 };
 
 Heading.h3 = Heading.withComponent('h3');
 
 Heading.h3.defaultProps = {
   color: 'grey80',
-  fontSize: 'size5'
+  fontSize: 'size5',
+  ...Heading.defaultProps
 };
 
 Heading.h4 = Heading.withComponent('h4');
 
 Heading.h4.defaultProps = {
   color: 'grey80',
-  fontSize: 'size4'
+  fontSize: 'size4',
+  ...Heading.defaultProps
 };
 
 Heading.h5 = styled(Heading.withComponent('h5'))`
-  font-weight: ${props => props.theme.fontWeights.medium};
   line-height: ${props => props.theme.lineHeights.large};
 `;
 
 Heading.h5.defaultProps = {
   color: 'grey100',
-  fontSize: 'size2'
+  fontSize: 'size2',
+  fontWeight: 'medium'
 };
 
 Heading.h6 = styled(Heading.withComponent('h6'))`
-  font-weight: ${props => props.theme.fontWeights.regular};
   line-height: ${props => props.theme.lineHeights.large};
 `;
 
 Heading.h6.defaultProps = {
   color: 'grey80',
-  fontSize: 'size2'
+  fontSize: 'size2',
+  fontWeight: 'regular'
 };
 
 export default Heading;
