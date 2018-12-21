05378998eee3bf7bb764c373bff30e056c6bae28
Euan Lau <euan@catchandrelease.tv>
2018-12-21 14:25:05 -0800

    Update Pagination story and make it interactive

--- stories/paginations.stories.js
+++ stories/paginations.stories.js
@@ -5,20 +5,39 @@ import { Pagination, Flex } from '../src';
 
 const stories = storiesOf('Pagination', module);
 
+class PaginationContainer extends React.Component {
+  state = {
+    currentPage: 1,
+    totalPages: 10
+  };
+
+  paginate = newPage => {
+    this.setState({ currentPage: newPage });
+  };
+
+  render() {
+    const { currentPage, totalPages } = this.state;
+
+    return (
+      <Pagination {...{ currentPage, totalPages }} paginate={this.paginate} />
+    );
+  }
+}
+
 stories.add('default', () => (
   <Flex mt="100px" justifyContent="center">
-    <Pagination currentPage={1} totalPages={10} />
+    <PaginationContainer />
   </Flex>
 ));
 
 stories.add('small', () => (
   <Flex mt="100px" justifyContent="center">
-    <Pagination size="small" currentPage={1} totalPages={10} />
+    <PaginationContainer />
   </Flex>
 ));
 
 stories.add('medium', () => (
   <Flex mt="100px" justifyContent="center">
-    <Pagination size="medium" currentPage={1} totalPages={10} />
+    <PaginationContainer />
   </Flex>
 ));
