3c1bd56c0fa0c93ac71b3ad03131e59266c50258
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2019-02-06 10:05:14 -0500

    Allow checkbox labels to use default alignment
     - Default value for align-items is start, removed the explicit set of
    center and the override of start.
    
    [#160968652]

--- src/Checkbox/CheckboxLabel.js
+++ src/Checkbox/CheckboxLabel.js
@@ -4,7 +4,6 @@ import Flex from '../Flex';
 
 const CheckboxLabel = styled(Flex.withComponent('label'))`
   cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
-  align-items: start;
   line-height: ${({ theme }) => theme.lineHeights.small};
 `;
 
