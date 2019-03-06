b1c2cefa300c71c4a8d2893aab08fe4b7cb6f3d5
Damian Galarza <damian@catchandrelease.com>
2019-03-06 16:56:58 -0500

    Update Accordion styles
    * Remove Accordion Component. An Accordion will not have a border or
    border-radius by default. Instead, the consumer should compose with
    Card.
    
    * Update storybook Accordion example to show how you might build out an
    accordion using this composition.

--- src/AccordionSection/StyledAccordionPanel.js
+++ src/AccordionSection/StyledAccordionPanel.js
@@ -5,7 +5,6 @@ import createWithComponent from '../utils/createWithComponent';
 
 const StyledAccordionPanel = styled(createWithComponent(Box, 'div'))`
   border-bottom: 1px solid ${props => props.theme.borderColors.default};
-  padding: ${props => props.theme.space.regular};
 
   section:last-child & {
     border-bottom: none;
