24d71cddc9be7b6cf9ba436027be89d92279c074
Damian Galarza <damian@catchandrelease.com>
2019-03-06 16:31:31 -0500

    Support additional grid props on Grid component
    We should be able to align things within the grid.

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled';
 import {
+  alignContent,
   alignItems,
+  gridColumnGap,
   gridGap,
+  gridRowGap,
   gridTemplateColumns,
+  justifyContent,
   justifyItems
 } from 'styled-system';
 
@@ -12,16 +16,24 @@ import createWithComponent from '../utils/createWithComponent';
 const Grid = styled(
   createWithComponent(Box, 'div', {
     propTypes: {
+      ...alignContent.propTypes,
+      ...gridColumnGap.propTypes,
       ...gridGap.propTypes,
       ...gridTemplateColumns.propTypes,
+      ...gridRowGap.propTypes,
+      ...justifyContent.propTypes,
       ...justifyItems.propTypes
     }
   })
 )`
   display: grid;
+  ${alignContent};
   ${alignItems};
+  ${gridColumnGap};
   ${gridGap};
+  ${gridRowGap};
   ${gridTemplateColumns};
+  ${justifyContent};
   ${justifyItems};
 `;
 
