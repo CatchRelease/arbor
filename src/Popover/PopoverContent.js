92035b2abf8b9c0e856c7c0a567cd5296e080588
Michael Dungan <41678+xxx@users.noreply.github.com>
2019-03-07 13:41:46 -0800

    update PopoverContent to use elevation2 as its box shadow (#96)
    * update PopoverContent to use elevation2 as its box shadow
    
    [finish #164466979]
    
    * box shadows are already being exported from the theme under another name
    use that.
    
    [finish #164466979]

--- src/Popover/PopoverContent.js
+++ src/Popover/PopoverContent.js
@@ -1,13 +1,10 @@
 import styled from '@emotion/styled';
-import { transparentize } from 'polished';
 
 import Card from '../Card';
 
 const PopoverContent = styled(Card)`
   background: ${props => props.theme.colors.white};
-  box-shadow: 0 4px 8px
-      ${props => transparentize(0.9, props.theme.colors.black)},
-    0 0 1px ${props => props.theme.colors.grey20};
+  box-shadow: ${props => props.theme.shadows.elevation2};
   min-height: 40px;
   min-width: 200px;
   overflow: hidden;
