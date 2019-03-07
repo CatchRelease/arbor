020965fd476b20c6a3aff785d3a3137fb97b7235
Damian Galarza <damian@catchandrelease.com>
2019-03-07 15:27:32 -0500

    Remove forced styles on HeaderNote

--- src/AccordionSection/AccordionHeader.js
+++ src/AccordionSection/AccordionHeader.js
@@ -4,26 +4,22 @@ import PropTypes from 'prop-types';
 import StyledAccordionHeader from './StyledAccordionHeader';
 import StyledAccordionHeading from './StyledAccordionHeading';
 import StyledAccordionHeadingButton from './StyledAccordionHeadingButton';
-import Text from '../Text';
 import Icon from '../Icon';
 
 const AccordionHeaderIcon = ({ isOpen }) => (
-  <Icon ml="regular" mr="small" name="caret" rotation={isOpen ? null : '270'} />
+  <Icon mr="small" name="caret" rotation={isOpen ? null : '270'} />
 );
 
 AccordionHeaderIcon.propTypes = {
   isOpen: PropTypes.bool.isRequired
 };
 
-const AccordionHeaderNote = ({ note }) =>
-  note && (
-    <Text mr="regular" fontSize="size4" textAlign="right" color="text.muted">
-      {note}
-    </Text>
-  );
-
 const AccordionHeader = ({ isOpen, note, onClick, panelId, text }) => (
-  <StyledAccordionHeader alignItems="center" justifyContent="space-between">
+  <StyledAccordionHeader
+    alignItems="center"
+    justifyContent="space-between"
+    px="regular"
+  >
     <StyledAccordionHeading fontWeight="medium" mr="regular">
       <StyledAccordionHeadingButton
         alignItems="center"
@@ -36,7 +32,7 @@ const AccordionHeader = ({ isOpen, note, onClick, panelId, text }) => (
         {text}
       </StyledAccordionHeadingButton>
     </StyledAccordionHeading>
-    <AccordionHeaderNote note={note} />
+    {note}
   </StyledAccordionHeader>
 );
 
