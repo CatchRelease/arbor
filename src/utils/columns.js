b16a25106eab091b195979bdbb28bbb7b80820d2
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2019-05-13 15:11:08 -0400

    Add styled-system style functions for columns
    [#165869160]

---
+++ src/utils/columns.js
@@ -0,0 +1,37 @@
+import { style } from 'styled-system';
+
+export const columnCount = style({
+  prop: 'columnCount'
+});
+
+export const columnGap = style({
+  prop: 'columnGap',
+  key: 'space'
+});
+
+export const columnRule = style({
+  prop: 'columnRule',
+  key: 'borders'
+});
+
+export const columnRuleColor = style({
+  prop: 'columnRuleColor',
+  key: 'colors'
+});
+
+export const columnRuleStyle = style({
+  prop: 'columnRuleStyle'
+});
+
+export const columnRuleWidth = style({
+  prop: 'columnRuleWidth',
+  key: 'space'
+});
+
+export const columnWidth = style({
+  prop: 'columnWidth'
+});
+
+export const columns = style({
+  prop: 'columns'
+});
