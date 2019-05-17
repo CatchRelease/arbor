62d5432848a973b7ded214a63bd7057d1c6c6dea
mpd <mpd@jesters-court.net>
2019-05-16 17:13:26 -0700

    use the theme to get the font weight

--- src/AccordionSection/StyledAccordionHeadingText.js
+++ src/AccordionSection/StyledAccordionHeadingText.js
@@ -6,7 +6,7 @@ export const StyledText = styled(Flex)`
   background: none;
   border: none;
   font-size: inherit;
-  font-weight: normal;
+  font-weight: ${props => props.theme.fontWeights.regular};
   padding: 0;
 `;
 
