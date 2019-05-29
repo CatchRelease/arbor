f39f1eab1961a75c4c144c9751823a2343d0a3f6
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2019-05-29 17:23:47 -0400

    Update color palette
    [Finishes #166330539]
    
    * Start expanding theme into multiple files
    * Mark old colors as deprecated
    * Add accessible palette
    * Add primary colors
    * Add secondary colors
    * Add monochrome colors
    * Add intent colors
    * Regenerate test screenshots with updated theme

---
+++ src/theme/colors/monochrome.js
@@ -0,0 +1,21 @@
+export const black = '#000000';
+export const white = '#FFFFFF';
+
+export const greys = {
+  grey10: '#F8F9F9',
+  grey20: '#F2F2F2',
+  grey30: '#EBECEC',
+  grey40: '#E0E1E2',
+  grey50: '#BEBEC0',
+  grey60: '#989A9C',
+  grey70: '#78797D',
+  grey80: '#5B5D61',
+  grey90: '#25272D',
+  grey100: '#0F0F10'
+};
+
+export default {
+  ...greys,
+  black,
+  white
+};
---
+++ src/theme/colors/primary.js
@@ -0,0 +1,21 @@
+export const blue = '#4263EB';
+export const cyan = '#0D7F91';
+export const green = '#29863A';
+export const orange = '#C55205';
+export const pink = '#D6336C';
+export const purple = '#7048E8';
+export const red = '#D93838';
+export const teal = '#098562';
+export const yellow = '#A36900';
+
+export default {
+  blue,
+  cyan,
+  green,
+  orange,
+  pink,
+  purple,
+  red,
+  teal,
+  yellow
+};
