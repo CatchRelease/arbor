5c64301be7cabcd5011f84d9335409b08a6ee45f
Damian Galarza <damian@catchandrelease.com>
2019-07-17 11:34:01 -0400

    Expand color palette for Avatar
    [Finishes #167325871]

--- stories/avatar.stories.js
+++ stories/avatar.stories.js
@@ -10,24 +10,49 @@ const stories = storiesOf('Avatar', module);
 stories.addDecorator(withKnobs);
 
 stories.add('default', () => (
-  <Flex justifyContent="space-around" width="200px" mt="100px">
+  <Flex justifyContent="space-around" mt="100px">
     <Avatar
-      name={text('Avatar 1', 'Alfred Pennyworth')}
+      name={text('Avatar 1', 'Aruce Wayne')}
       subtle={boolean('subtle', false)}
       size={select('Size', sizes, 'default')}
     />
     <Avatar
-      name={text('Avatar 2', 'Batman')}
+      name={text('Avatar 2', 'Bruce Wayne')}
       subtle={boolean('subtle', false)}
       size={select('Size', sizes, 'default')}
     />
     <Avatar
-      name={text('Avatar 3', 'Bruce Wayne')}
+      name={text('Avatar 3', 'Cruce Wayne')}
       subtle={boolean('subtle', false)}
       size={select('Size', sizes, 'default')}
     />
     <Avatar
-      name={text('Avatar 4', 'Dick Grayson')}
+      name={text('Avatar 4', 'Druce Wayne')}
+      subtle={boolean('subtle', false)}
+      size={select('Size', sizes, 'default')}
+    />
+    <Avatar
+      name={text('Avatar 5', 'Eruce Wayne')}
+      subtle={boolean('subtle', false)}
+      size={select('Size', sizes, 'default')}
+    />
+    <Avatar
+      name={text('Avatar 6', 'Fruce Wayne')}
+      subtle={boolean('subtle', false)}
+      size={select('Size', sizes, 'default')}
+    />
+    <Avatar
+      name={text('Avatar 7', 'Gruce Wayne')}
+      subtle={boolean('subtle', false)}
+      size={select('Size', sizes, 'default')}
+    />
+    <Avatar
+      name={text('Avatar 8', 'Hruce Wayne')}
+      subtle={boolean('subtle', false)}
+      size={select('Size', sizes, 'default')}
+    />
+    <Avatar
+      name={text('Avatar 9', 'Iruce Wayne')}
       subtle={boolean('subtle', false)}
       size={select('Size', sizes, 'default')}
     />
