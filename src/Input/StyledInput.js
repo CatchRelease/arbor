ca2bf55d6c8146bed1dba6ca5209d0e8d0d49bc8
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2019-07-18 17:15:06 -0400

    Abstract styles from Input to its own file
    [#167264066]
    
    * Will need these input styles in other components that are not actual
    inputs

--- src/Input/StyledInput.js
+++ src/Input/StyledInput.js
@@ -1,41 +1,8 @@
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
-import { placeholder, transparentize } from 'polished';
-import { maxWidth } from 'styled-system';
-
-const baseStyles = ({ theme }) => css`
-  border-radius: ${theme.radii.small};
-  border: ${theme.borderWidth.small} solid ${theme.colors.border.default};
-  box-sizing: border-box;
-  color: ${theme.colors.text.default};
-  font-family: ${theme.brandFont};
-  font-size: ${theme.fontSizes.input.default};
-  line-height: ${theme.lineHeights.small};
-  margin: ${theme.space.smallest} 0;
-  padding: ${theme.space.smaller};
-  width: 100%;
-
-  @media (max-width: ${theme.breakpointsMap.md}) {
-    font-size: ${theme.fontSizes.input.mobile};
-  }
-
-  ${placeholder({ color: theme.colors.grey60 })};
-
-  &:focus {
-    border: ${theme.borderWidth.small} solid ${theme.colors.intent.brand.dark};
-    box-shadow: 0 0 8px ${transparentize(0.9, theme.colors.intent.brand.light)};
-    outline: none;
-  }
-
-  &:disabled {
-    background: ${theme.colors.background.default};
-  }
-
-  &:required {
-    box-shadow: none;
-  }
-`;
+import { display, maxWidth } from 'styled-system';
+import inputStyles from './inputStyles';
 
 const largeStyles = ({ theme, large }) =>
   large &&
@@ -44,13 +11,15 @@ const largeStyles = ({ theme, large }) =>
   `;
 
 const StyledInput = styled.input`
-  ${baseStyles};
+  ${inputStyles};
+  ${display};
   ${largeStyles};
   ${maxWidth};
 `;
 
 StyledInput.propTypes = {
   large: PropTypes.bool,
+  ...display.propTypes,
   ...maxWidth.propTypes
 };
 
