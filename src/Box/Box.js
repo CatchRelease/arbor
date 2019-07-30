60735a68fafa357482169bcf2be8d496f0490cf1
Damian Galarza <damian@catchandrelease.com>
2019-07-30 11:23:19 -0400

    Use Text as basic component
    All components render text in some way. It makes sense to have
    typography related props available everywhere and have proper default of
    font-family etc..

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -2,55 +2,39 @@ import styled from '@emotion/styled';
 import {
   borders,
   boxShadow,
-  color,
   display,
   gridArea,
   height,
   maxHeight,
-  maxWidth,
   minHeight,
-  minWidth,
-  overflow,
   position,
-  space,
-  textAlign,
   width
 } from 'styled-system';
 
-const Box = styled.div`
+import Text from '../Text';
+
+const Box = styled(Text.withComponent('div'))`
   box-sizing: border-box;
   ${borders};
   ${boxShadow};
-  ${color};
   ${display};
   ${height};
   ${gridArea};
   ${maxHeight};
-  ${maxWidth};
   ${minHeight};
-  ${minWidth};
-  ${overflow};
   ${position};
-  ${space};
-  ${textAlign};
   ${width};
 `;
 
 Box.propTypes = {
   ...borders.propTypes,
   ...boxShadow.propTypes,
-  ...color.propTypes,
   ...display.propTypes,
   ...gridArea.propTypes,
   ...height.propTypes,
   ...maxHeight.propTypes,
-  ...maxWidth.propTypes,
   ...minHeight.propTypes,
-  ...minWidth.propTypes,
-  ...overflow.propTypes,
   ...position.propTypes,
-  ...space.propTypes,
-  ...textAlign.propTypes,
   ...width.propTypes
 };
 
