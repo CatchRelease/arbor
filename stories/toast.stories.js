eb82f1c9e5e0619c16106f24400e8785d944d36b
Anthony Costanzo <a.e.j.costanzo@gmail.com>
2019-10-21 17:38:34 -0400

    Setup toast example
    [#169235664]
    
    TODO: Figure out animations

--- stories/toast.stories.js
+++ stories/toast.stories.js
@@ -1,26 +1,77 @@
-import React from 'react';
+/** @jsx jsx */
+import { Global, css, jsx } from '@emotion/core';
 import { ToastContainer, toast } from 'react-toastify';
 import { storiesOf } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
 
-import 'react-toastify/dist/ReactToastify.css';
-
-import { Box, Button, Heading } from '../src';
+import { Box, Button, Heading, Link, ToastAlert } from '../src';
+import INTENTS from '../src/Alert/intents';
 
 const stories = storiesOf('Toast', module);
-stories.addDecorator(withKnobs);
 
 stories.add('default', () => {
-  const notify = () =>
-    toast('Well, well, well, well, well. That was six wells');
+  const details = (
+    <div>
+      That&lsquo;s six wells.&nbsp;
+      <Link href="https://www.dailymotion.com/video/x5xlg93" target="_blank">
+        Look, a link!
+      </Link>
+    </div>
+  );
+
+  const message = 'Well, well, well, well, well...';
+
+  const notify = intent => {
+    const toastId = toast(
+      <ToastAlert
+        details={details}
+        intent={intent}
+        message={message}
+        onClose={() => toast.dismiss(toastId)}
+      />
+    );
+  };
+
+  const arborToastCSS = css`
+    .Toastify {
+      .arbor-toast-container {
+        display: grid;
+        height: 0px;
+        justify-content: center;
+        left: 0;
+        position: fixed;
+        top: 0;
+        width: 100%;
+      }
+
+      .arbor-toast {
+        margin-top: 16px;
+        max-width: 90vw;
+      }
+    }
+  `;
 
   return (
     <Box as="section" p="regular">
       <Heading.h1>Toast</Heading.h1>
 
-      <Button onClick={notify}>toasty!</Button>
+      <Global styles={arborToastCSS} />
 
-      <ToastContainer />
+      {INTENTS.map(intent => (
+        <Box mb="smaller" textAlign="right">
+          <Button intent={intent} onClick={() => notify(intent)}>
+            {`${intent} toast!`}
+          </Button>
+        </Box>
+      ))}
+
+      <ToastContainer
+        className="arbor-toast-container"
+        toastClassName="arbor-toast"
+        closeButton={false}
+        hideProgressBar
+        position={toast.POSITION.TOP_CENTER}
+        preventExitTransition
+      />
     </Box>
   );
 });
