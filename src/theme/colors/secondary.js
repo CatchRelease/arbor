00dd5d689a6f8987bd0c976807fe18903a4c384c
Mayrav Herman <mayravherman@gmail.com>
2019-11-05 16:55:15 -0800

    Update green and teal secondary colors (#194)
    * Update green and teal secondary colors
    
    * Update tests

--- src/theme/colors/secondary.js
+++ src/theme/colors/secondary.js
@@ -1,11 +1,11 @@
 export const blue = '#4B9BFF';
 export const cyan = '#5FC8FF';
-export const green = '#50D2A0';
+export const green = '#9BE169';
 export const orange = '#FFB46E';
 export const pink = '#FF73C3';
 export const purple = '#AF78FF';
 export const red = '#FF5F5F';
-export const teal = '#9BE169';
+export const teal = '#50D2A0';
 export const yellow = '#FFD76E';
 
 export default {
