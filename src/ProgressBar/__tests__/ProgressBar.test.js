2d4a6ba4efe1985984b6fe0ff1cb9dabcecde415
Michael Dungan <41678+xxx@users.noreply.github.com>
2019-11-25 13:31:38 -0800

    for progress bar end icons, if the bar is less than 100 percent, show the icon in grey rather than the default color. (#201)
    The text is always the default color.
    
    [finish #169963693]

--- src/ProgressBar/__tests__/ProgressBar.test.js
+++ src/ProgressBar/__tests__/ProgressBar.test.js
@@ -4,10 +4,22 @@ import mountWithTheme from '../../../utils/mountWithTheme';
 import ProgressBar from '../ProgressBar';
 
 describe('<ProgressBar />', () => {
-  it('renders the description list properly', () => {
+  it('renders the progress bar properly', () => {
     const progressBar = mountWithTheme(
       <ProgressBar
         percentage={42}
+        iconEndName="calendar"
+        showPercentage={false}
+      />
+    );
+
+    expect(progressBar).toMatchSnapshot();
+  });
+
+  it('renders the progress bar properly at 100% with an icon', () => {
+    const progressBar = mountWithTheme(
+      <ProgressBar
+        percentage={100}
         iconEndName="calendar"
         showPercentage={false}
       />
