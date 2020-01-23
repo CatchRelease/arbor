d5f7eafb219d1a4c598c5a415d836396038eb6d7
Paul Venable <paul@catchandrelease.com>
2020-01-23 11:57:29 -0800

    Add Controlled Tabs storybook story
    This basically behaves just like the Default (uncontrolled) story, but
    the source demonstrates using Tabs in controlled mode.
    
    [#170346780]

--- stories/tabs.stories.js
+++ stories/tabs.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 
 import notes from './tabs.md';
@@ -56,5 +56,62 @@ stories.add(
       </Tabs>
     </Box>
   ),
+  { notes: { markdown: notes } }
+);
+
+stories.add(
+  'Controlled',
+  () => {
+    const [activeTabId, setActiveTabId] = useState('tab-1');
+
+    return (
+      <Box as="section" p="regular">
+        <Heading.h1>Controlled Tabs</Heading.h1>
+
+        <Tabs activeTabId={activeTabId}>
+          <Tab id="tab-1" title="Tab 1" onClick={() => setActiveTabId('tab-1')}>
+            <Box bg="palette.blue.lighter" p="largest">
+              Tab 1 Content
+            </Box>
+          </Tab>
+          <Tab
+            id="tab-2"
+            title={titleAsComponent}
+            onClick={() => setActiveTabId('tab-2')}
+          >
+            <Box bg="palette.green.lighter" p="largest">
+              Tab 2 Content
+            </Box>
+          </Tab>
+          <Tab id="tab-3" title="Tab 3" onClick={() => setActiveTabId('tab-3')}>
+            <Box bg="palette.red.lighter" id="foo-3" p="largest">
+              Tab 3 Content
+            </Box>
+          </Tab>
+          <Tab
+            id="tab-4"
+            title="Image tab"
+            onClick={() => setActiveTabId('tab-4')}
+          >
+            <Box>
+              <img
+                alt="example of issue "
+                src="http://fakeimage.herokuapp.com/400x400.png?color=yellow"
+              />
+            </Box>
+          </Tab>
+          <Tab
+            id="alert"
+            title="alert!"
+            onClick={() => alertTabOnclick(() => setActiveTabId('alert'))}
+          >
+            <Box bg="palette.red.lighter" id="foo-4" p="largest">
+              This will be rendered with an alert also
+            </Box>
+          </Tab>
+        </Tabs>
+      </Box>
+    );
+  },
   { notes: { markdown: notes } }
 );
