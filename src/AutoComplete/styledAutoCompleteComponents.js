4de39cd79bc65f307bd68cd3138600d2072e4cc2
mpd <mpd@jesters-court.net>
2020-02-17 09:36:04 -0800

    export the styled autocomplete subcomponents, so that we can tweak their styling without wholesale copy/pasting
    [#171095538]

---
+++ src/AutoComplete/styledAutoCompleteComponents.js
@@ -0,0 +1,35 @@
+import StyledAutoComplete from './StyledAutoComplete';
+import StyledAutoCompleteClearIndicator from './StyledAutoCompleteClearIndicator';
+import StyledAutoCompleteContainer from './StyledAutoCompleteContainer';
+import StyledAutoCompleteControl from './StyledAutoCompleteControl';
+import StyledAutoCompleteDropdownIndicator from './StyledAutoCompleteDropdownIndicator';
+import StyledAutoCompleteIndicatorsContainer from './StyledAutoCompleteIndicatorsContainer';
+import StyledAutoCompleteInput from './StyledAutoCompleteInput';
+import StyledAutoCompleteMenu from './StyledAutoCompleteMenu';
+import StyledAutoCompleteMenuList from './StyledAutoCompleteMenuList';
+import StyledAutoCompleteMultiValue from './StyledAutoCompleteMultiValue';
+import StyledAutoCompleteNoOptionsMessage from './StyledAutoCompleteNoOptionsMessage';
+import StyledAutoCompletePlaceholder from './StyledAutoCompletePlaceholder';
+import StyledAutoCompleteOption from './StyledAutoCompleteOption';
+import StyledAutoCompleteValueContainer from './StyledAutoCompleteValueContainer';
+
+export const subComponents = {
+  ClearIndicator: StyledAutoCompleteClearIndicator,
+  Container: StyledAutoCompleteContainer,
+  Control: StyledAutoCompleteControl,
+  DropdownIndicator: StyledAutoCompleteDropdownIndicator,
+  IndicatorsContainer: StyledAutoCompleteIndicatorsContainer,
+  Input: StyledAutoCompleteInput,
+  Menu: StyledAutoCompleteMenu,
+  MenuList: StyledAutoCompleteMenuList,
+  MultiValue: StyledAutoCompleteMultiValue,
+  NoOptionsMessage: StyledAutoCompleteNoOptionsMessage,
+  Placeholder: StyledAutoCompletePlaceholder,
+  Option: StyledAutoCompleteOption,
+  ValueContainer: StyledAutoCompleteValueContainer
+};
+
+export default {
+  ...subComponents,
+  StyledAutoComplete
+};
