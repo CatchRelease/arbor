0b80e6a26c3a2a4bf6e91b0a35f72013d8f0e0a6
mpd <mpd@jesters-court.net>
2020-04-30 12:04:09 -0700

    expose paletteColor as a prop for StyledAutoCompleteMultiValue, so there can be an easier-to-configure default.
    [#172137985]

--- dist/AutoComplete/StyledAutoCompleteMultiValue.js
+++ dist/AutoComplete/StyledAutoCompleteMultiValue.js
@@ -19,6 +19,14 @@ var _Icon = _interopRequireDefault(require("../Icon"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
@@ -30,8 +38,9 @@ function _extends() { _extends = Object.assign || function (target) { for (var i
 var StyledAutoCompleteMultiValue = function StyledAutoCompleteMultiValue(_ref) {
   var data = _ref.data,
       label = _ref.data.label,
+      paletteColor = _ref.paletteColor,
       removeProps = _ref.removeProps;
-  var paletteColor = (0, _utils.colorForString)(label, Object.keys(_colors.palette));
+  var color = paletteColor === null ? (0, _utils.colorForString)(label, Object.keys(_colors.palette)) : paletteColor;
 
   var iconEnd = _react["default"].createElement(_Icon["default"], _extends({
     "aria-label": "remove",
@@ -44,7 +53,7 @@ var StyledAutoCompleteMultiValue = function StyledAutoCompleteMultiValue(_ref) {
 
   return _react["default"].createElement(_Badge["default"], _objectSpread({
     iconEnd: iconEnd,
-    paletteColor: paletteColor,
+    paletteColor: color,
     subtle: true,
     variant: 'pill'
   }, data), label);
@@ -58,7 +67,11 @@ StyledAutoCompleteMultiValue.propTypes = {
     onClick: _propTypes["default"].func.isRequired,
     onMouseDown: _propTypes["default"].func.isRequired,
     onTouchEnd: _propTypes["default"].func.isRequired
-  }).isRequired
+  }).isRequired,
+  paletteColor: _propTypes["default"].oneOf([''].concat(_toConsumableArray(Object.keys(_colors.palette))))
+};
+StyledAutoCompleteMultiValue.defaultProps = {
+  paletteColor: null
 };
 var _default = StyledAutoCompleteMultiValue;
 exports["default"] = _default;
