2814589aa573fcbe555075cab87c0dec0ebe77ae
Michael Carey <mikecx@gmail.com>
2020-05-12 15:15:26 -0700

    Silence a lot of the test warnings around js specs.

--- utils/createWithTheme.js
+++ utils/createWithTheme.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { CacheProvider } from '@emotion/core';
+import createCache from '@emotion/cache';
 import { create } from 'react-test-renderer';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '../src/theme';
 
+const myCache = createCache();
+myCache.compat = true;
+
 export default (Component) =>
-  create(<ThemeProvider theme={theme}>{Component}</ThemeProvider>);
+  create(
+    <CacheProvider value={myCache}>
+      <ThemeProvider theme={theme}>{Component}</ThemeProvider>
+    </CacheProvider>
+  );
