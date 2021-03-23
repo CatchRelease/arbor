24157b10978b93588f793a0832e94c79d5f5aa8f
Paul Venable <paul@catchandrelease.com>
2021-03-23 09:11:50 -0700

    Add addon-essentials; disable controls and docs
    - Controls: None are configured yet.
    - Docs: None are configured yet, and switching to the Docs tab breaks
      the Canvas tab, which fails to render anything.
    
    [#177401579]

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,13 @@ const path = require('path');
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        controls: false,
+        docs: false
+      }
+    },
     '@storybook/addon-links',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
