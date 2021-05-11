c0a3ed47ef2c176ae6ac711e59645729adae51cb
Paul Venable <paul@catchandrelease.com>
2021-05-11 11:03:29 -0700

    Update node modules

--- src/Toast/toast.js
+++ src/Toast/toast.js
@@ -3,17 +3,19 @@ import { toast as reactToastifyToast } from 'react-toastify';
 import { ToastAlert } from '../Alert';
 import INTENTS from '../Alert/intents';
 
-const buildToast = (intent) => (message, details, toastifyOptions = {}) => {
-  const toastId = reactToastifyToast(
-    <ToastAlert
-      details={details}
-      intent={intent}
-      message={message}
-      onClose={() => reactToastifyToast.dismiss(toastId)}
-    />,
-    toastifyOptions
-  );
-};
+const buildToast =
+  (intent) =>
+  (message, details, toastifyOptions = {}) => {
+    const toastId = reactToastifyToast(
+      <ToastAlert
+        details={details}
+        intent={intent}
+        message={message}
+        onClose={() => reactToastifyToast.dismiss(toastId)}
+      />,
+      toastifyOptions
+    );
+  };
 
 const toast = INTENTS.reduce(
   (memo, intent) => ({ ...memo, [intent]: buildToast(intent) }),
