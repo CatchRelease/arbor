95c9145f5eebc797a1284f2a94d4815433bae691
Paul Venable <paul@catchandrelease.com>
2021-08-05 12:13:12 -0700

    Update node modules

--- dist/Heading/Heading.d.ts
+++ dist/Heading/Heading.d.ts
@@ -14,16 +14,16 @@ declare const _default: import("@emotion/styled").StyledComponent<{
 } & {
     theme?: Theme | undefined;
 } & Props, {}, {}> & {
-    H1: FC<import("type-fest/source/simplify").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
-    H2: FC<import("type-fest/source/simplify").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
-    H3: FC<import("type-fest/source/simplify").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
-    H4: FC<import("type-fest/source/simplify").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
-    H5: import("@emotion/styled").StyledComponent<import("type-fest/source/simplify").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>> & {
+    H1: FC<import("type-fest").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
+    H2: FC<import("type-fest").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
+    H3: FC<import("type-fest").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
+    H4: FC<import("type-fest").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>>>;
+    H5: import("@emotion/styled").StyledComponent<import("type-fest").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>> & {
         children?: import("react").ReactNode;
     } & {
         theme?: Theme | undefined;
     }, {}, {}>;
-    H6: import("@emotion/styled").StyledComponent<import("type-fest/source/simplify").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>> & {
+    H6: import("@emotion/styled").StyledComponent<import("type-fest").Simplify<import("type-fest").Except<Props, "fontSize"> & Partial<Pick<Props, "fontSize">>> & {
         children?: import("react").ReactNode;
     } & {
         theme?: Theme | undefined;
