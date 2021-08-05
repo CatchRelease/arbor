e0cf572e0d4f9994d9d8728659ffac7d731b8471
Paul Venable <paul@catchandrelease.com>
2021-08-05 12:13:12 -0700

    Update node modules

--- dist/AccordionSection/StyledAccordionHeading.d.ts
+++ dist/AccordionSection/StyledAccordionHeading.d.ts
@@ -1,5 +1,5 @@
 export default StyledHeading;
-declare const StyledHeading: import("@emotion/styled").StyledComponent<import("type-fest/source/simplify").Simplify<import("type-fest").Except<{
+declare const StyledHeading: import("@emotion/styled").StyledComponent<import("type-fest").Simplify<import("type-fest").Except<{
     fontSize: "size4" | "size1" | "size2" | "size3" | "size5" | "size6" | "size7" | "size8" | "size9";
     fontWeight?: "bold" | "medium" | "regular" | undefined;
     mb: string;
