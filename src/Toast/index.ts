b1aae1113e4427b32556efa3da8390b1dbe0ccd4
Ted Whang <twhang.dev@gmail.com>
2021-08-18 15:13:51 -0700

    Export ToastOptions type

--- src/Toast/index.ts
+++ src/Toast/index.ts
@@ -1,3 +1,4 @@
 export { default } from './toast';
 export { default as toast } from './toast';
 export { default as ToastContainer } from './ToastContainer';
+export { ToastOptions } from 'react-toastify';
