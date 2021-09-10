cbac3042b336940302c36750cc68461874950bec
Paul Venable <paul@catchandrelease.com>
2021-09-10 15:25:15 -0700

    Convert monochrome.js to TypeScript
    This causes the greys to be added to the type declaration for
    `colors.monochrome`.
    
    This should enable scenarios like this to type-check:
    
    ```
    import { colors } from '@catchandrelease/arbor';
    
    console.log(colors.monochrome.grey10);
    ```
    
    [#179378243]

--- dist/theme/colors/monochrome.d.ts
+++ dist/theme/colors/monochrome.d.ts
@@ -1,19 +1,29 @@
-export const black: "#000000";
-export const white: "#FFFFFF";
-export namespace greys {
-    const grey10: string;
-    const grey20: string;
-    const grey30: string;
-    const grey40: string;
-    const grey50: string;
-    const grey60: string;
-    const grey70: string;
-    const grey80: string;
-    const grey90: string;
-    const grey100: string;
-}
-declare namespace _default {
-    export { black };
-    export { white };
-}
+export declare const black = "#000000";
+export declare const white = "#FFFFFF";
+export declare const greys: {
+    grey10: string;
+    grey20: string;
+    grey30: string;
+    grey40: string;
+    grey50: string;
+    grey60: string;
+    grey70: string;
+    grey80: string;
+    grey90: string;
+    grey100: string;
+};
+declare const _default: {
+    black: string;
+    white: string;
+    grey10: string;
+    grey20: string;
+    grey30: string;
+    grey40: string;
+    grey50: string;
+    grey60: string;
+    grey70: string;
+    grey80: string;
+    grey90: string;
+    grey100: string;
+};
 export default _default;
