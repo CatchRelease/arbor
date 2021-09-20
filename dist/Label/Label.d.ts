3c8fa6b9109c04df0d728d3ae9db56b3d6da3291
Paul Venable <paul@catchandrelease.com>
2021-09-20 13:55:11 -0700

    Convert Label to TypeScript

--- dist/Label/Label.d.ts
+++ dist/Label/Label.d.ts
@@ -1,5 +1,6 @@
-export default Label;
+/// <reference types="react" />
 declare const Label: import("@emotion/styled").StyledComponent<{
     theme?: import("@emotion/react").Theme | undefined;
     as?: import("react").ElementType<any> | undefined;
 }, import("react").DetailedHTMLProps<import("react").LabelHTMLAttributes<HTMLLabelElement>, HTMLLabelElement>, {}>;
+export default Label;
