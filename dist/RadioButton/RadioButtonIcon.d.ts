5792d5f318e4573ef433793701d65de5616f300b
Paul Venable <paul@catchandrelease.com>
2021-10-11 10:53:48 -0700

    Update dist files
    [#179890667]

--- dist/RadioButton/RadioButtonIcon.d.ts
+++ dist/RadioButton/RadioButtonIcon.d.ts
@@ -1,14 +1,6 @@
+import { VFC } from 'react';
+declare type Props = {
+    checked?: boolean;
+};
+declare const RadioButtonIcon: VFC<Props>;
 export default RadioButtonIcon;
-declare function RadioButtonIcon({ checked }: {
-    checked: any;
-}): import("@emotion/react/jsx-runtime").JSX.Element | null;
-declare namespace RadioButtonIcon {
-    namespace propTypes {
-        const checked: PropTypes.Requireable<boolean>;
-    }
-    namespace defaultProps {
-        const checked_1: boolean;
-        export { checked_1 as checked };
-    }
-}
-import PropTypes from "prop-types";
--- dist/RadioButton/RadioButtonIcon.js
+++ dist/RadioButton/RadioButtonIcon.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("@emotion/react/jsx-runtime");
-const prop_types_1 = __importDefault(require("prop-types"));
 const Icon_1 = __importDefault(require("../Icon"));
 const iconFontSize = '16px';
 const RadioButtonIcon = ({ checked }) => {
@@ -12,14 +11,5 @@ const RadioButtonIcon = ({ checked }) => {
         return (0, jsx_runtime_1.jsx)(Icon_1.default, { name: "radio-checked", fontSize: iconFontSize }, void 0);
     }
     return null;
-};
-RadioButtonIcon.propTypes = {
-    /**
-     * Whether or not the checkbox is checked.
-     */
-    checked: prop_types_1.default.bool
-};
-RadioButtonIcon.defaultProps = {
-    checked: false
 };
 exports.default = RadioButtonIcon;
