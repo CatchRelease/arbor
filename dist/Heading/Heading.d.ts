1feff2b8d7a0b9b223d26a8e7f61e3d0f53924c1
Paul Venable <paul@catchandrelease.com>
2021-10-12 08:41:19 -0700

    Update dist files
    [#179907276]

--- dist/Heading/Heading.d.ts
+++ dist/Heading/Heading.d.ts
@@ -1,9 +1,8 @@
 import { ElementType, FC } from 'react';
 import { Theme } from '@emotion/react';
-import { FontSize, FontWeight } from '../theme/typography';
+import { FontWeight } from '../theme/typography';
 import { TextProps } from '../Text';
 export declare type Props = TextProps & {
-    fontSize: FontSize;
     fontWeight?: FontWeight;
     mb?: string;
 };
@@ -17,7 +16,6 @@ declare const _default: import("@emotion/styled").StyledComponent<Omit<{
 } & {
     theme?: Theme | undefined;
 } & Omit<import("react").HTMLAttributes<HTMLElement>, "color"> & import("styled-system").ColorProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, string | number | symbol> & import("styled-system").FontSizeProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, string | number | symbol> & import("styled-system").FontWeightProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, string | number | symbol> & import("styled-system").LineHeightProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, string | number | symbol> & import("styled-system").MaxWidthProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, import("csstype").Property.MaxWidth<import("styled-system").TLengthStyledSystem>> & import("styled-system").MinWidthProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, import("csstype").Property.MinWidth<import("styled-system").TLengthStyledSystem>> & import("styled-system").OverflowProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>> & import("styled-system").SpaceProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>, string | number | symbol> & import("styled-system").TextAlignProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>> & import("../utils/textDecoration").TextDecorationProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>> & import("../utils/textTransform").TextTransformProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>> & import("../utils/textOverflow").TextOverflowProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>> & import("../utils/whiteSpace").WhitespaceProps<Required<import("styled-system").Theme<import("styled-system").TLengthStyledSystem>>> & {
-    fontSize: "size4" | "size1" | "size2" | "size3" | "size5" | "size6" | "size7" | "size8" | "size9";
     fontWeight?: "bold" | "medium" | "regular" | undefined;
     mb?: string | undefined;
 }, {}, {}> & {
