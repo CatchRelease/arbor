340f03e3282a660022126ec9d8d3f39ff628381a
Paul Venable <paul@catchandrelease.com>
2021-12-20 11:38:59 -0800

    Convert Tooltip to TypeScript
    [#180628455]

--- dist/Tooltip/Tooltip.d.ts
+++ dist/Tooltip/Tooltip.d.ts
@@ -1,25 +1,10 @@
+import { ComponentProps, ReactNode, VFC } from 'react';
+import Tippy from '@tippyjs/react/headless';
+declare type TippyProps = ComponentProps<typeof Tippy>;
+declare type Props = {
+    children: TippyProps['children'];
+    content?: ReactNode;
+    title?: ReactNode;
+};
+declare const Tooltip: VFC<Props>;
 export default Tooltip;
-declare function Tooltip({ title, content, children, render, ...props }: {
-    [x: string]: any;
-    title: any;
-    content: any;
-    children: any;
-    render: any;
-}): import("@emotion/react/jsx-runtime").JSX.Element;
-declare namespace Tooltip {
-    namespace propTypes {
-        const children: PropTypes.Validator<PropTypes.ReactElementLike>;
-        const content: PropTypes.Requireable<string | number | boolean | {} | PropTypes.ReactElementLike | PropTypes.ReactNodeArray>;
-        const title: PropTypes.Requireable<string | number | boolean | {} | PropTypes.ReactElementLike | PropTypes.ReactNodeArray>;
-        const render: PropTypes.Requireable<(...args: any[]) => any>;
-    }
-    namespace defaultProps {
-        const content_1: null;
-        export { content_1 as content };
-        const title_1: null;
-        export { title_1 as title };
-        const render_1: null;
-        export { render_1 as render };
-    }
-}
-import PropTypes from "prop-types";
